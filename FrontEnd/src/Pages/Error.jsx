16123088d8d6238c39a7ad7bfad07692964afbf4
Rionecnirp <alex.ben-larbi@hotmail.fr>
2025-08-12 19:08:59 +0200

    12/08/2025 - 19h10 : - Ajout du CSS pour le responsive téléphone - Ajout du CSS pour la page Error (je l'avais oublié)

--- FrontEnd/src/Pages/Error.jsx
+++ FrontEnd/src/Pages/Error.jsx
@@ -1,14 +1,23 @@
 import { NavLink } from 'react-router-dom';
 import Footer from '../Components/Footer/Footer'
+import '../Style/Error.css'
+import Navigation from '../Components/Navigation/Navigation';
 
 const Error = () => {
     return (
-        <main>
+        <>
+            <Navigation />
+            <main className='Error'>
             <h1>404</h1>
-            <p>Oups ! La page que vous demandez n'existe pas.</p>
-            <NavLink to="/">Retournez sur la page d’accueil</NavLink>
+            <h2>Oups ! La page que vous demandez n'existe pas.</h2>
+            <NavLink to="/">
+                <span>
+                    Retournez sur la page d’accueil
+                </span>
+            </NavLink>
+            </main>
             <Footer />
-        </main>
+        </>
         
     );
 };
